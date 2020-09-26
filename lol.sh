e3dbf7ae5dbc54d1b6f21c3027a59cfa19782eee
Kekok55 <71831121+Kekok55@users.noreply.github.com>
2020-09-26 10:49:47 +0700

    Create lol.sh

---
+++ lol.sh
@@ -0,0 +1,41 @@
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;m'
+sleep 1
+echo
+toilet -f big -F gay By Kekok
+echo $green"Selamat Datang"
+echo
+sleep 2
+echo $yellow"Silahkan Pilih Tools Nya :"
+echo
+echo $cyan"1.) Stabilkan Jaringan"
+echo
+echo $blue"0.) Keluar Aja !!!"
+echo
+echo $white
+read -p "Pilihmana : " Bro
+
+if [ $Bro = 1 ] || [ $Bro = 1 ]
+then
+clear
+toilet -f big -F gay Kekok
+echo $green"Stabilkan Jaringan By Kekok"
+sleep 2
+ping -s1000 1.1.1.1
+fi
+
+if [ $Bro = 0 ] || [ $Bro = 0 ]
+then
+clear
+echo $yellow
+figlet "Bye"
+echo $blue"Terima Kasih Goblog"
+sleep 3
+exit
+fi
